afbb1d4e60b7dc35a08d9ff0bf73ea5f7089cf52
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2023-05-02 13:54:45 +0300

    Create reusable component for the card

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, Image } from "react-native";
+import { Card, Button } from "react-native-elements";
+import Deck from "./src/components/Deck";
+import { DATA } from "./src/mockData";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Deck>
+        {DATA.map((card) => (
+          <Card key={card.id}>
+            <Image style={styles.image} source={{ uri: card.uri }} />
+            <Text style={styles.text}>{card.text}</Text>
+            <Button
+              icon={{ name: "code" }}
+              backgroundColor="03A9F4"
+              title="View now"
+            />
+          </Card>
+        ))}
+      </Deck>
     </View>
   );
 }
@@ -13,8 +26,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  text: {
+    marginBottom: 10,
+  },
+  image: {
+    height: 100,
+    marginBottom: 10,
   },
 });
---
+++ src/components/Deck.js
@@ -0,0 +1,7 @@
+import { View, Animated } from "react-native";
+
+const Deck = ({ children }) => {
+  return <View>{children}</View>;
+};
+
+export default Deck;
